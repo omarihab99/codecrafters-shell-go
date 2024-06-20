1acfe0e91de68d23a3e6497ce30bea694cd5676c
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-06-20 20:37:59 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"bufio"
 	"strings"
-	// Uncomment this block to pass the first stage
 	"fmt"
 	"os"
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	for{
+	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err == nil {
-			fmt.Println(strings.Split(input, "\n")[0] + ": command not found")
+			input = strings.TrimSpace(input)
+			slices := strings.Split(input, " ")
+			handleCommand(slices[0], slices[1:])
 		}
 	}
-	
+}
+// handleCommand is a function that takes a command and its arguments as input.
+//
+// Parameters:
+// - command: a string representing the command to be executed.
+// - args: a slice of strings representing the arguments passed to the command.
+//
+// Return type: None.
+func handleCommand(command string, args []string) {
+	if command == "exit" && len(args) > 0 && args[0] == "0" {
+		os.Exit(0)
+	} else {
+		fmt.Println(command + ": command not found")
+	}
 }
