a0eb0ecf491842a86cbb2a21cd6b8802dd07dde2
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-06-20 23:16:22 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"slices"
 	"strings"
@@ -16,31 +17,49 @@ type Command struct {
 }
 
 func (c Command) Execute() {
-	if c.Name == "exit" && len(c.Args) > 0 && c.Args[0] == "0" {
+	if c.Name == "exit" && len(c.Args) > 0 && c.Args[0] == "0" { // exit
 		os.Exit(0)
-	} else if c.Name == "echo" {
+	} else if c.Name == "echo" { // echo
 		fmt.Println(strings.Join(c.Args, " "))
-	} else if c.Name == "type" {
-		handeType(c)
-	} else {
-		fmt.Println(c.Name + ": command not found")
-	}
-}
-func handeType(c Command) {
-	subcommand := c.Args[0]
-	if slices.Contains(builtinCommand, subcommand) {
-		fmt.Println(subcommand + " is a shell builtin")
+		return
+	} else if c.Name == "type" { // type
+		fmt.Println(c.CommandType)
 		return
 	}
+	fp := searchPath(c.Name) 
+	if fp != "" { // executable
+		cmd := exec.Command(fp, c.Args...)
+		output, err := cmd.CombinedOutput()
+		if err == nil {
+			fmt.Println(strings.TrimSpace(string(output)))
+			return
+		}
+	}
+	fmt.Println(c.Name + ": command not found") // command not found
+}
+func handeType(command string, Args []string) string {
+	subcommand := command
+	if len(Args) > 0 {
+		subcommand = Args[0]
+	}
+	if slices.Contains(builtinCommand, subcommand) {
+		return subcommand + " is a shell builtin"
+	}
+	fp := searchPath(subcommand)
+	if fp != "" {
+		return fp
+	}
+	return subcommand + ": not found"
+}
+func searchPath(subcommand string) string {
 	paths := strings.Split(os.Getenv("PATH"), ":")
 	for _, path := range paths {
 		filePath := filepath.Join(path, subcommand)
 		if _, err := os.Stat(filePath); err == nil {
-			fmt.Println(filePath)
-			return
+			return filePath
 		}
 	}
-	fmt.Println(subcommand + ": not found")
+	return ""
 }
 
 var builtinCommand = []string{"cd", "pwd", "echo", "exit", "type"}
@@ -59,10 +78,7 @@ func main() {
 }
 
 func CreateCommand(command string, args []string) Command {
-	var commandType string = "unknown"
-	if slices.Contains(builtinCommand, command) {
-		commandType = "builtin"
-	}
+	commandType := handeType(command , args)
 	return Command{
 		Name:        command,
 		Args:        args,
