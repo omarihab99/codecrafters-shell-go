6f2fb70a5da1ed0c6ff786b641cf4983f1a8675d
omarihab99 <omar.ihab.mohamed99@gmail.com>
2024-06-20 22:05:09 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,8 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
+
+type Command struct {
+	Name        string
+	Args        []string
+	CommandType string
+}
+
+func (c Command) Execute() {
+	if c.Name == "exit" && len(c.Args) > 0 && c.Args[0] == "0" {
+		os.Exit(0)
+	} else if c.Name == "echo" {
+		fmt.Println(strings.Join(c.Args, " "))
+	} else if c.Name == "type" {
+		subcommand := c.Args[0]
+		if slices.Contains(builtinCommand, subcommand) {
+			fmt.Println(subcommand + " is a shell builtin")
+		} else {
+			fmt.Println(subcommand + ": not found")
+		}
+	} else {
+		fmt.Println(c.Name + ": command not found")
+	}
+}
+
+var builtinCommand = []string{"cd", "pwd", "echo", "exit", "type"}
 
 func main() {
 	for {
@@ -14,24 +40,20 @@ func main() {
 		if err == nil {
 			input = strings.TrimSpace(input)
 			slices := strings.Split(input, " ")
-			handleCommand(slices[0], slices[1:])
+			command := CreateCommand(slices[0], slices[1:])
+			command.Execute()
 		}
 	}
 }
 
-// handleCommand is a function that takes a command and its arguments as input.
-//
-// Parameters:
-// - command: a string representing the command to be executed.
-// - args: a slice of strings representing the arguments passed to the command.
-//
-// Return type: None.
-func handleCommand(command string, args []string) {
-	if command == "exit" && len(args) > 0 && args[0] == "0" {
-		os.Exit(0)
-	} else if command == "echo" {
-		fmt.Println(strings.Join(args, " "))
-	} else {
-		fmt.Println(command + ": command not found")
+func CreateCommand(command string, args []string) Command {
+	var commandType string = "unknown"
+	if slices.Contains(builtinCommand, command) {
+		commandType = "builtin"
+	}
+	return Command{
+		Name:        command,
+		Args:        args,
+		CommandType: commandType,
 	}
 }
